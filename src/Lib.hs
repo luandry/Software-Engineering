f97adef11fe37f9ad2b2d0d645f8c665d5365f8a
Luan Williams <willialu@tcd.ie>
2018-09-27 15:55:11 +0100

    Second commit, how nice

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,6 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
 someFunc :: IO ()
+someOtherFunc :: IO ()
 someFunc = putStrLn "A quick Haskell program, also testing if my github works"
+someOtherFunc = putStrLn "Second test to see if I can merge/commit"
