a8e72648d505405aad586963184d75c90b6a2e68
Luan Williams <willialu@tcd.ie>
2018-09-28 16:39:35 +0100

    Got a test file working, and a few little other tweaks

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,12 @@
+import BTree
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception (evaluate)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+
+ describe "Binary Tree functionality testing." $ do
+  it "Create tree node" $ do
+   let t = Node 3 (Node 2 Leaf Leaf) (Node 4 Leaf Leaf)
+   (treeDepth t) `shouldBe` 2
