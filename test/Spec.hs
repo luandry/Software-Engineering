735fbc04cfc47925060f57173d775a5c10d2daa9
Luan Williams <willialu@tcd.ie>
2018-09-29 10:09:00 +0100

    Wrote a test for my isSortedTree function

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,7 +6,16 @@ import Control.Exception (evaluate)
 main :: IO ()
 main = hspec $ do
 
- describe "Binary Tree functionality testing." $ do
+ describe "Binary Tree Depth Tester" $ do
   it "Create tree node" $ do
    let t = Node 3 (Node 2 Leaf Leaf) (Node 4 Leaf Leaf)
    (treeDepth t) `shouldBe` 2
+
+
+ describe "Binary Tree is Sorted Tester" $ do
+  it "Create tree node" $ do
+    isSortedTree (Node 5 (Node 4 Leaf Leaf) (Node 6 Leaf Leaf)) minBound maxBound `shouldBe` True
+
+
+ 
+
