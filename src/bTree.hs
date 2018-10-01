e68f9fc5b7113730c1d345ede9b88ce6cce78b94
Luan Williams <willialu@tcd.ie>
2018-10-01 12:43:15 +0100

    Wrote a new treeMax Function, and wrote a test for it and the addNewMax function

--- src/bTree.hs
+++ src/bTree.hs
@@ -1,4 +1,6 @@
+
 module BTree where
+import Control.Applicative
 
 data Tree = Leaf | Node Int Tree Tree deriving Show
 
@@ -15,7 +17,26 @@ isSortedTree (Node x leftSubtree rightSubtree) minVal maxVal =
  in x >= minVal && x < maxVal && leftSorted && rightSorted
 
 addNewMax :: Tree -> Tree
-
 addNewMax Leaf = Node 0 Leaf Leaf
 addNewMax (Node x t1 Leaf) = Node x t1 (Node (x+1) Leaf Leaf)
 addNewMax (Node x t1 t2) = Node x t1 (addNewMax t2)
+
+treeSum :: Tree -> Int
+treeSum Leaf = 0
+treeSum (Node x leftSubtree rightSubtree) = 
+ x + (treeSum leftSubtree) + (treeSum rightSubtree)
+
+treeMax :: Tree -> Int 
+treeMax Leaf = 0
+treeMax (Node x leftSubtree rightSubtree) = 
+ maximum [x, treeMax leftSubtree, treeMax rightSubtree]
+
+
+--has :: Eq a => a -> Tree a -> Bool
+--has _ Leaf = False
+--has e (Node v leftSubtree rightSubtree) = v == e || has e leftSubtree || has e rightSubtree 
+
+--lca :: Ord a => a -> a -> Tree a -> Maybe a
+--lca _ _ Leaf = Nothing
+--lca m n (Node v leftSubtree rightSubtree) | has m (Node v leftSubtree Nil) && has n (Node v Leaf rightSubtree) = Just v
+--                                          | otherwise = lca m n leftSubtree <|> lca m n rightSubtree
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,18 +3,32 @@ import Test.Hspec
 import Test.QuickCheck
 import Control.Exception (evaluate)
 
+
 main :: IO ()
 main = hspec $ do
 
  describe "Binary Tree Depth Tester" $ do
-  it "Create tree node" $ do
+  it "Depth is Functioning" $ do
    let t = Node 3 (Node 2 Leaf Leaf) (Node 4 Leaf Leaf)
    (treeDepth t) `shouldBe` 2
 
 
  describe "Binary Tree is Sorted Tester" $ do
-  it "Create tree node" $ do
+  it "Is Sorted is Functioning" $ do
     isSortedTree (Node 5 (Node 4 Leaf Leaf) (Node 6 Leaf Leaf)) minBound maxBound `shouldBe` True
+
+
+ describe "Binary Tree Tree Sum Tester" $ do
+  it "Binary Tree Sum is Functioning" $ do
+   let t = Node 3 (Node 2 Leaf Leaf) (Node 4 Leaf Leaf)
+   (treeSum t) `shouldBe` 9
+
+ describe "Add New Max to Binary Tree Tester" $ do
+  it "The Add New Max is Functioning" $ do
+   let t = Node 3 (Node 2 Leaf Leaf) (Node 4 Leaf Leaf)
+   let u = (addNewMax t)
+   (treeMax u) `shouldBe` 5
+ 
 
 
  
